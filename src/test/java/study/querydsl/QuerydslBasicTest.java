c58cc739689c1656e57a3da47357a465957569a7
leeyoungchun <dldydcns123@gmail.com>
2022-05-02 19:36:53 +0900

    Case 문

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
@@ -432,6 +433,39 @@ public class QuerydslBasicTest {
         //then
     }
 
+    @Test
+    public void basicCase() throws Exception  {
+        //given
+        List<String> result = queryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+        //when
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+        //then
+    }
+
+    @Test
+    public void complexCase() throws Exception  {
+        //given
+        List<String> reseult = queryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+        //when
+        for (String s : reseult) {
+            System.out.println("s = " + s);
+        }
+        //then
+    }
 
 
 
