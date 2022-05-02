b6489b59ab627db643b4a7797a79c09d8238fc4b
leeyoungchun <dldydcns123@gmail.com>
2022-05-02 18:04:43 +0900

    검색 조건 쿼리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -71,7 +71,26 @@ public class QuerydslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
-
+    @Test
+    public void search() throws Exception  {
+        //given
+        Member findMember = queryFactory.selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+    @Test
+    public void searchAndParam() throws Exception  {
+        //given
+        Member findMember = queryFactory.selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 
 
 
