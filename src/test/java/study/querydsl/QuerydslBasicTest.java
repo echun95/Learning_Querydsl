bd847f9b7068791b01c3218664860a48ed7df690
leeyoungchun <dldydcns123@gmail.com>
2022-05-03 22:42:31 +0900

    프로젝션 결과 반환 - Projections를 이용한 dto 조회

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,8 @@ package study.querydsl;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
@@ -13,6 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.QTeam;
@@ -528,6 +532,97 @@ public class QuerydslBasicTest {
         }
         //then
     }
+
+    @Test
+    public void findDtoJPQL() throws Exception  {
+        //given
+        List<MemberDto> result = em.createQuery("select new study.querydsl.dto.MemberDto(m.username, m.age) " +
+                        "from Member m", MemberDto.class)
+                .getResultList();
+        //when
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+        //then
+    }
+
+    @Test
+    public void findDtoBySetter() throws Exception  {
+        //given
+        List<MemberDto> result = queryFactory
+                .select(Projections.bean(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        //when
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+        //then
+    }
+    @Test
+    public void findDtoByField() throws Exception  {
+        //given
+        List<MemberDto> result = queryFactory
+                .select(Projections.fields(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        //when
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+        //then
+    }
+    @Test
+    public void findDtoByConstructor() throws Exception  {
+        //given
+        List<MemberDto> result = queryFactory
+                .select(Projections.constructor(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        //when
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+        //then
+    }
+    @Test
+    public void findUserDto() throws Exception  {
+        QMember memberSub = new QMember("memberSub");
+        //given
+        List<UserDto> result = queryFactory
+                .select(Projections.fields(UserDto.class,
+//                        member.username.as("name"),
+                        ExpressionUtils.as(member.username,"name"),
+                        ExpressionUtils.as(JPAExpressions
+                                .select(memberSub.age.max())
+                                .from(memberSub),"age")
+                ))
+                .from(member)
+                .fetch();
+        //when
+        for (UserDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+        //then
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
